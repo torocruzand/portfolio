f78d5e4ed8ceb864d9509c11b1b19a76ea26219c
Andres <torocruzand@gmail.com>
2026-02-10 18:48:34 -0300

    chore: configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 }
 
 export default nextConfig
